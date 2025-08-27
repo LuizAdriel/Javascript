c6d7dc51d1a9271f523bb44a648fba81aad8ce67
Luiz Adriel <Luizgennio31@gmail.com>
2025-08-27 09:25:56 -0300

    versao calculadora idade

--- nodejs/modulos/app.js
+++ nodejs/modulos/app.js
@@ -1,4 +1,10 @@
-const saudacao = require('./meuModulo'); // Importando o módulo
+const modulo = require('./meuModulo'); // Importando o módulo
 
-const mensagem = saudacao('Joédio'); // Executando a função
+const calculadora_idade = require('./calculadora_idade')
+
+const mensagem = modulo.saudacao('Joédio'); // Executando a função
+const mensagemIdade = calculadora_idade.idade('Joédio', 1990);
+
 console.log(mensagem);
+console.log(idade2025);
+
